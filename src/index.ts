09febd407efbd9b6e0b6b9876035670f32fee48f
Ashish Patel <ashishsushilpatel@gmail.com>
2024-11-08 23:41:38 +0000

    feat: add formatDate method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-const print = (message = "Hello"): void => {
-    console.log(message)
+import { format, parseISO } from "date-fns"
+
+const formatDate = (date: string): string => {
+    const isoDate = parseISO(date)
+    const formattedDate = format(isoDate, "MMMM d, y")
+    return formattedDate
 }
 
-export {print}
+function truncate(str: string, len: number) {
+    for (let i = len - 2; i >= 0; i--) {
+        str = str.substring(0, i)
+    }
+
+    return str.trim()
+}
+
+export { formatDate, truncate }
